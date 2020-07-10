5bf021c7064be2b25093462ddc9aca5659894318
Fraiters <67577747+Fraiters@users.noreply.github.com>
2020-07-10 03:01:57 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
 #include<gl\glut.h>
 #include <math.h>
 #include <cstdlib>
+#include <iostream>
 #include "Ball.h"
 #include "Vars.h"
+
+using namespace std;
 
 //size of field
 int const width = 300;
 int const height = 300;
+bool control = true;
 
 float r_x = 110.0, r_y = 280.0, r_w = 80, r_h = 10.0; //coordinates and dimensions of the platform
 Ball ball(r_y, 8); //created an object of class Ball with a position in y and radius
@@ -36,9 +40,25 @@ void Draw()
 
 
 
+void Pick_system()
+{
+	int a;
+	cout << "Choose your system:" << endl;
+	cout << "1) Bot" << endl;
+	cout << "2) Manual control" << endl;
+	cout << "Press 1 or 2 and then press 's' or 'down' for start game" << endl;
+	cin >> a;
+	
+	if (a == 1)
+		control = true;
+		else if (a == 2) control = false;
+}
+
+
+
 void Bot()
 {
-	//If the unit is equipped with x, the unit is equipped with a box for completing the process 
+	//if the unit is equipped with x, the unit is equipped with a box for completing the process 
 	if (ball.active)
 	{
 	if (r_x <= 220)	
@@ -60,7 +80,8 @@ void Timer(int)
 	Draw();
 	
 	if(ball.active){
-		Bot();
+		if (control == true) 
+			Bot();
 		ball.move();
 		
 	}	
@@ -69,31 +90,32 @@ void Timer(int)
 
 
 
-
-
 void Keyboard(unsigned char key, int x, int y)
 {
 	
 	switch(key)
 	{
-		case 'a':
+		if (control == false)
 		{
-			if (r_x >= 0){
-				for (int i = 0; i < 10; i++)
-					--r_x;
-				break;
+			case 'a':
+			{
+				if (r_x >= 0){
+					for (int i = 0; i < 10; i++)
+						--r_x;
+					break;
+				}
+				else break;
 			}
-			else break;
+			case 'd':
+			{
+				if (r_x <= 220){
+					for (int i = 0; i < 10; i++)
+						++r_x;
+					break;
+				}
+				else break;
+		    }
 		}
-		case 'd':
-		{
-			if (r_x <= 220){
-				for (int i = 0; i < 10; i++)
-					++r_x;
-				break;
-			}
-			else break;
-	    }
 	    case 's':
 	    	if(!ball.active){
 	    		ball.active = true;
@@ -110,33 +132,37 @@ void Keyboard(unsigned char key, int x, int y)
   		
 }
 
+
 void SKeyboard(int key, int x, int y)
 {
 		switch(key)
-	{
-		case GLUT_KEY_LEFT:
+	{	
+		if (control == false)
 		{
-			if (r_x >= 0){
-				for (int i = 0; i < 10; i++)
-					--r_x;
-				break;
+			case GLUT_KEY_LEFT:
+			{
+				if (r_x >= 0){
+					for (int i = 0; i < 10; i++)
+						--r_x;
+					break;
+				}
+				else break;
 			}
-			else break;
+			case GLUT_KEY_RIGHT:
+			{
+				if (r_x <= 220){
+					for (int i = 0; i < 10; i++)
+						++r_x;
+					break;
+				}
+				else break;
+		    }
 		}
-		case GLUT_KEY_RIGHT:
-		{
-			if (r_x <= 220){
-				for (int i = 0; i < 10; i++)
-					++r_x;
-				break;
-			}
-			else break;
-	    }
 	    case GLUT_KEY_DOWN:
 	    	if(!ball.active){
 	    		ball.active = true;
-	    		ball.dx = 7;
-				ball.dy = -7;
+	    		ball.dx = 6;
+				ball.dy = -6;
 			}
 	}	
 	if (!ball.active)
@@ -159,15 +185,25 @@ int main(int argc, char** argv)
 	//argc - number of accessories in a commercially available key
 	//argv - description in full on the page
 {
+	Pick_system();
 	glutInit(&argc, argv); 	//global initialization	
+	
+	
+		
+	
 	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB); //tell how we will draw, GLUT_DOUBLE - use the drawing buffer and the calculation buffer GLUT_RGB - the colors are based on RGB
 	glutInitWindowSize(width, height); //window size initialization	
 	glutInitWindowPosition(100, 200); 	//window position initialization
 	glutCreateWindow("Arkanoid 2D"); //window creation
 	glutDisplayFunc(Draw); //draw
 	glutTimerFunc(33, Timer, 0);
+	
+	//if ( == false)
+	
 	glutKeyboardFunc(Keyboard);
 	glutSpecialFunc(SKeyboard);
+	
+	
 	Initialize(); //initialization
 	glutMainLoop();  //main cycle
 	return 0;
